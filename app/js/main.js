add6a6f32f77ae42c61b79b420b35d77fd894345
Sihula <serhiisihula@gmail.com>
2020-07-08 14:28:24 +0200

    add product-slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,19 @@
 $(function () {
+
+  
+  $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "12px",
+    normalFill: "#A0A0A0",
+    readOnly: true,
+  });
+
+  $('.product-slider__inner').slick({
+    dots:true,
+    arrows:false,
+    slidesToShow: 4,
+    slidesToScroll:2,
+  });
+
   var mixer = mixitup(".products__inner-box");
 });
